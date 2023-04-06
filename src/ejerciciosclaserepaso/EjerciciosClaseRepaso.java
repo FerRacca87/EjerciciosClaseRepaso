c4828ccb3d3a0bfd821a119bc90255d1061e3dad
Fernando Racca <fernandoracca87@gmail.com>
2023-04-06 20:27:17 -0300

    Ejercicio 2B

--- src/ejerciciosclaserepaso/EjerciciosClaseRepaso.java
+++ src/ejerciciosclaserepaso/EjerciciosClaseRepaso.java
@@ -1,11 +1,15 @@
 package ejerciciosclaserepaso;
 
 //@author Fernando Racca
+
+import java.util.Scanner;
+
 public class EjerciciosClaseRepaso {
 
     public static void main(String[] args) {
         //Ejercicio1();
-        Ejercicio2();
+        //Ejercicio2();
+        Ejercicio2Scanner();
     }
 
     private static void Ejercicio1() {
@@ -17,31 +21,59 @@ public class EjerciciosClaseRepaso {
 
         System.out.println("El equipo sumo " + puntosTotales + " puntos");
     }
-    
-    private static void Ejercicio2(){
+
+    private static void Ejercicio2() {
         int golesEquipo1 = 2;
         int golesEquipo2 = 3;
         int equipoGanador = 0;
         boolean empate = false;
-        
-        if (golesEquipo1 > golesEquipo2){
+
+        if (golesEquipo1 > golesEquipo2) {
             equipoGanador = 1;
-        } else if (golesEquipo2 > golesEquipo1){
+        } else if (golesEquipo2 > golesEquipo1) {
             equipoGanador = 2;
         } else {
             empate = true;
         }
-        
-        if(empate){
+
+        if (empate) {
             System.out.println("Es un empate entre ambos equipos");
         } else {
             System.out.println("Gano el equipo " + equipoGanador);
-        }     
+        }
     }
     
-    private static void Ejercicio3(){
+    
+    private static void Ejercicio2Scanner() {
         
+        Scanner in = new Scanner(System.in);
+                
+        int golesEquipo1, golesEquipo2;
+        int equipoGanador = 0;
+        boolean empate = false;
         
+        System.out.print("Ingrese los goles del equipo 1: ");
+        golesEquipo1 = in.nextInt();
+        System.out.print("Ingrese los goles del equipo 2: ");
+        golesEquipo2 = in.nextInt();
+
+        if (golesEquipo1 > golesEquipo2) {
+            equipoGanador = 1;
+        } else if (golesEquipo2 > golesEquipo1) {
+            equipoGanador = 2;
+        } else {
+            empate = true;
+        }
+
+        if (empate) {
+            System.out.println("Es un empate entre ambos equipos");
+        } else {
+            System.out.println("Gano el equipo " + equipoGanador);
+        }
+    }
+
+    private static void Ejercicio3() {
+
     }
 
 }
